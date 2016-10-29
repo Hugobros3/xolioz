bd2c144fb355fc90bb1f3a9965a651b078b3882b
Hugo Srevilled <hugo@xol.io>
2016-10-29 13:08:48 +0200

    3d model for torch

--- src/io/xol/dogez/mods/items/DZTorch.java
+++ src/io/xol/dogez/mods/items/DZTorch.java
@@ -7,8 +7,8 @@ import io.xol.chunkstories.api.item.ItemType;
 import io.xol.chunkstories.api.rendering.RenderingInterface;
 import io.xol.chunkstories.api.rendering.lightning.SpotLight;
 import io.xol.chunkstories.api.world.World;
+import io.xol.chunkstories.core.item.renderers.ObjViewModelRenderer;
 import io.xol.chunkstories.item.ItemPile;
-import io.xol.chunkstories.item.renderer.DefaultItemRenderer;
 import io.xol.engine.math.lalgb.Matrix4f;
 import io.xol.engine.math.lalgb.Vector3f;
 import io.xol.engine.math.lalgb.Vector4f;
@@ -24,31 +24,30 @@ public class DZTorch extends Item{
 		itemRenderer = new TorchItemRenderer(this);
 	}
 
-	class TorchItemRenderer extends DefaultItemRenderer{
+	class TorchItemRenderer extends ObjViewModelRenderer{
 
 		public TorchItemRenderer(Item item) {
-			super(item);
+			super(item, "./models/misc/torch/torch.obj", "./models/misc/torch/torchOff.png");
 		}
-
 		
 		@Override
 		public void renderItemInWorld(RenderingInterface context, ItemPile pile, World world, Location location, Matrix4f handTransformation)
 		{
 			super.renderItemInWorld(context, pile, world, location, handTransformation);
 			//Has it got an user ?
-			if(pile.inventory != null)
+			if(pile.getInventory() != null)
 			{
-				if(pile.inventory.holder != null)
+				if(pile.getInventory().getHolder() != null)
 				{
-					if(pile.inventory.holder instanceof EntityRotateable)
+					if(pile.getInventory().getHolder() instanceof EntityRotateable)
 					{
 						Vector4f vec4 = new Vector4f(0.0, 0.0, 0.0, 1.0);
 						Matrix4f.transform(handTransformation, vec4, vec4);
 						
 						Vector3f vec3 = new Vector3f(vec4.x, vec4.y, vec4.z);
 						
-						context.addLight(new SpotLight(new Vector3f(1f, 1f, 0.9f).scale(1.5f), vec3, 45f, 35f, ((EntityRotateable) pile.inventory.holder).getDirectionLookingAt().castToSimplePrecision()));
-						context.addLight(new SpotLight(new Vector3f(1f, 1f, 0.9f).scale(0.5f), vec3, 95f, 35f, ((EntityRotateable) pile.inventory.holder).getDirectionLookingAt().castToSimplePrecision()));
+						context.addLight(new SpotLight(new Vector3f(1f, 1f, 0.9f).scale(1.5f), vec3, 45f, 35f, ((EntityRotateable) pile.getInventory().holder).getDirectionLookingAt().castToSimplePrecision()));
+						context.addLight(new SpotLight(new Vector3f(1f, 1f, 0.9f).scale(0.5f), vec3, 95f, 35f, ((EntityRotateable) pile.getInventory().holder).getDirectionLookingAt().castToSimplePrecision()));
 					}
 				}
 			}
